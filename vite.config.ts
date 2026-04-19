2ee0330d2b05c57016c490aeb99c0f45adc6b39c
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-04-19 16:07:31 +0100

    prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite"; //
 
 export default defineConfig({
+  base: "/D-Rahim/", // Set the base path for GitHub Pages
   plugins: [
     tailwindcss(), // This makes Tailwind "see" your classes
   ],
