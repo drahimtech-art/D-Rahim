c3c9a67f5a4ca00046171612af2db2927909a898
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-05-02 18:22:04 +0100

    bulding moblie view made get in touch page

--- src/Components/GetInTouch/EnterDetailsAndMap.tsx
+++ src/Components/GetInTouch/EnterDetailsAndMap.tsx
@@ -1,68 +1,74 @@
 function EnterDetailsAndMap() {
   return (
-    <div className="flex lg:pl-10 lg:pr-10 pl-5 pr-5 mt-15 lg:gap-10 gap-5">
-      <div className="w-[60%] max-w-232">
-        <div className=" flex flex-col max-w-232 gap-1.5">
+    <div className="flex lg:pl-10 lg:pr-10 pl-5 pr-5 sm:mt-15 mt-10 lg:gap-10 gap-5">
+      <div className="sm:w-[60%] sm:max-w-232">
+        <span className="ourWorkMenuText fontPoppins font-semibold sm:hidden">
+          <h5>Enter Details</h5>
+        </span>
+        <div className=" flex flex-col sm:max-w-232 sm:gap-1.5 gap-5 sm:mt-0 mt-10">
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Full Name
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Job Title
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Business Email
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Phone Number
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Company Name
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Tell us about your project
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="h-41.75 sm:h-full border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
           <span className=" font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               How did you find D'rahim tech innovation?
             </h5>
-            <input className="border-b-2 border-gray-600 w-full  pb-7 mt-6"></input>
+            <input className="h-41.75 sm:h-full border-2 sm:border-0 sm:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 sm:pb-7 sm:mt-6 rounded-2xl sm:rounded-none"></input>
           </span>
         </div>
         <div className="mt-10">
-          <span className="flex max-w-201.75 ">
-            <input className="w-6 h-6 text-green-400" type="radio"></input>
+          <span className="flex w-full items-center sm:max-w-201.75 ">
+            <input
+              className="sm:w-6 sm:h-6 w-10 h-10 text-green-400"
+              type="radio"
+            ></input>
             <h5 className=" text-[16px] ml-2 ">
               Keep this button selected to give consent for us to collect and
               process the personal information you've submitted, in line with
               our Privacy Policy.
             </h5>
           </span>
-          <span className="flex items-center justify-center mt-10 bg-green-600 w-36 h-12.5 rounded-full">
-            <h5 className="text-center ourWorkHeadMenu text-gray-200 font-medium font-inter">
+          <span className="flex ml-auto mr-auto sm:p-0 pl-7.5 pr-7.5 p-2.5 items-center justify-center mt-10 bg-green-600 sm:w-36 sm:h-12.5 w-fit rounded-full">
+            <h5 className="text-center min22 text-gray-200 font-medium font-inter">
               Submit
             </h5>
           </span>
         </div>
       </div>
-      <div className="w-[40%] ml-auto">
+      <div className="w-[40%] ml-auto hidden sm:block">
         <span className="flex justify-center w-full">
           <h5 className="font-semibold logoMainText">Find Us On The Map</h5>
         </span>
--- src/Components/GetInTouch/GetInTouchHeadText.tsx
+++ src/Components/GetInTouch/GetInTouchHeadText.tsx
@@ -6,14 +6,14 @@ function GetInTouchHeadText() {
   }
   return (
     <>
-      <div className="lg:pl-10 lg:pr-10 pl-5 pr-5 lg:mt-30 mt-10">
+      <div className="lg:pl-10 lg:pr-10 pl-5 pr-5 sm:mt-30 mt-10">
         <span className="block">
           <h5 className="font-size-heading fontPoppins font-semibold">
             Need Help With a Project?
           </h5>
         </span>
         <span className="block sm:mt-15 mt-10">
-          <h5 className="ourTermHeadText w-[80%] max-w-209.75 ">
+          <h5 className="max20px w-full font-bold sm:font-normal sm:w-[80%] max-w-209.75 ">
             <strong
               className="text-green-400 border-b-2 border-green-400 pointer"
               onClick={bookACall}
@@ -23,19 +23,19 @@ function GetInTouchHeadText() {
             At Your Convenience, Or Fill Out The Form And We'll Get In Touch.
           </h5>
         </span>
-        <div className="mt-15 ">
-          <h5 className="ourWorkHeadMenu font-bold">I'm interested in</h5>
-          <div className="flex lg:gap-10 gap-5 mt-2">
-            <span className="  w-[40%] max-w-81.5 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
-              <h5>Digital Product Design</h5>
+        <div className="sm:mt-15 mt-10">
+          <h5 className="min22 font-bold">I'm interested in</h5>
+          <div className="flex flex-col sm:flex-row lg:gap-10 gap-5 mt-5">
+            <span className="  pl-5 pr-5 sm:pl-0 sm:pr-0 w-fit sm:w-[30%] sm:max-w-81.5 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
+              <h5>Moblie Design</h5>
             </span>
-            <span className="  w-[30%] max-w-61.25 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
+            <span className="pl-5 pr-5 sm:pl-0 sm:pr-0 w-fit sm:w-[30%] sm:max-w-61.25 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
               <h5>Website Design</h5>
             </span>
-            <span className="  w-[30%] max-w-45 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
+            <span className="pl-5 pr-5 sm:pl-0 sm:pr-0  w-fit sm:w-[30%] sm:max-w-45 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
               <h5>Branding</h5>
             </span>
-            <span className="  w-[30%] max-w-32.5 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
+            <span className="pl-5 pr-5 sm:pl-0 sm:pr-0  w-fit sm:w-[30%] sm:max-w-32.5 flex justify-center items-center  h-12.5 font-inter font-medium ourWorkHeadMenu     border text-black border-black hover:bg-secondary-green hover:text-gray-200 hover:border-green-500 transition-all  rounded-full overflow-hidden whitespace-nowrap pointer">
               <h5>Other</h5>
             </span>
           </div>
