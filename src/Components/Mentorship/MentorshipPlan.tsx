923bee4668838a61b2dc9ff401d6a0decdbb6e3b
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-05-15 16:09:47 +0100

    modifing font sizes

--- src/Components/Mentorship/MentorshipPlan.tsx
+++ src/Components/Mentorship/MentorshipPlan.tsx
@@ -4,20 +4,20 @@ import High from "./PlansCard/High";
 function MentorshipPlan() {
   //
   return (
-    <div className="lg:pl-10 lg:pr-10 pr-5 pl-5 sm:mt-20 mt-10">
+    <div className="lg:pl-10 lg:pr-10 pr-5 pl-5 lg:mt-20 mt-10">
       <span className="block">
         <h5 className=" font-semibold fontPoppins font-size-heading">
           Ready? Choose Mentorship Plan
         </h5>
       </span>
-      <div className="sm:h-158.75 sm:mt-15 mt-10 flex flex-col gap-10 sm:flex-row sm:gap-8.75 justify-around">
-        <span className="w-full sm:w-111.25 h-141.75 sm:mt-17">
+      <div className="lg:h-158.75 lg:mt-15 mt-10 flex flex-col gap-10 lg:flex-row lg:gap-8.75 justify-around">
+        <span className="w-full lg:w-111.25 h-141.75 lg:mt-17">
           <Lowest />
         </span>
-        <span className="w-full sm:w-111.25 h-151.25 sm:mt-7.5">
+        <span className="w-full lg:w-111.25 h-151.25 lg:mt-7.5">
           <Meduim />
         </span>
-        <span className="w-full sm:w-111.25 h-158.75 ">
+        <span className="w-full lg:w-111.25 h-158.75 ">
           <High />
         </span>
       </div>
