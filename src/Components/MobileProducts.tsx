09c28f70c4da8cc929e3e13eeb69617d75ba2746
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-05-18 10:23:52 +0100

    removed pop-up background blur

--- src/Components/MobileProducts.tsx
+++ src/Components/MobileProducts.tsx
@@ -1,3 +1,16 @@
+type OurWorkData = {
+  image: string;
+  text: string;
+  subText?: string;
+  popUpHeadding: string;
+  popHeadText: string;
+  capabilities: string[];
+  duration: string;
+  team: string[];
+  location: string;
+  industry: string[];
+  endText: string;
+};
 function MobileProducts() {
   return <></>;
 }
