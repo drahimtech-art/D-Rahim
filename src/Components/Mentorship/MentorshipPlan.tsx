9f525d67242f93a0e3efb856c0e0b2ce01f08819
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-05-23 02:40:00 +0100

    fixing moblie mentorship plan payment card max height bug

--- src/Components/Mentorship/MentorshipPlan.tsx
+++ src/Components/Mentorship/MentorshipPlan.tsx
@@ -4,20 +4,20 @@ import High from "./PlansCard/High";
 function MentorshipPlan() {
   //
   return (
-    <div className="lg:pl-10 lg:pr-10 pr-5 pl-5 lg:mt-20 mt-10">
+    <div className="min-[1000px]:pl-10 min-[1000px]:pr-10 pr-5 pl-5 min-[1000px]:mt-20 mt-10">
       <span className="block">
         <h5 className=" font-semibold fontPoppins font-size-heading">
           Ready? Choose Mentorship Plan
         </h5>
       </span>
-      <div className="lg:h-158.75 lg:mt-15 mt-10 flex flex-col gap-10 lg:flex-row lg:gap-8.75 justify-around">
-        <span className="w-full lg:w-111.25 h-141.75 lg:mt-17">
+      <div className="min-[1000px]:h-158.75 min-[1000px]:mt-15 mt-10 flex flex-col gap-10 min-[1000px]:flex-row min-[1000px]:gap-8.75 justify-around">
+        <span className="w-full min-[1000px]:w-111.25 min-[1000px]:h-141.75 h-fit min-[1000px]:mt-17">
           <Lowest />
         </span>
-        <span className="w-full lg:w-111.25 h-151.25 lg:mt-7.5">
+        <span className="w-full min-[1000px]:w-111.25 min-[1000px]:h-151.25 h-fit min-[1000px]:mt-7.5">
           <Meduim />
         </span>
-        <span className="w-full lg:w-111.25 h-158.75 ">
+        <span className="w-full min-[1000px]:w-111.25 min-[1000px]:h-158.75 h-fit ">
           <High />
         </span>
       </div>
