9a340594953310ecd499e7d2eca7b9411430aebb
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-05-25 11:35:24 +0100

    modifing menu ux added pointer curser for clickable feedback

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -25,7 +25,7 @@ function Menu(props: MenuControl) {
   }
   return (
     <div className="w-full h-screen  fixed z-50 sm:w-75 sm:right-5 sm:h-fit sm:pl-5 sm:pr-5 sm:pt-16 sm:pb-30 sm:rounded-3xl sm:top-16 top-0 p-7 bg-[#020e02]">
-      <div className="flex items-center sm:hidden">
+      <div className="flex items-center  sm:hidden">
         <div className="flex text-gray-200 ">
           <div className="w-7  sm:w-8 h-10">
             <img src={Logo} alt="logo"></img>
@@ -46,37 +46,37 @@ function Menu(props: MenuControl) {
       </div>
       <div className="w-full mt-20 sm:mt-0 flex flex-col gap-3">
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-gray-200 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-gray-200 font-inter font-semibold "
           onClick={work}
         >
           <h5>Work</h5>
         </span>
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-gray-200 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-gray-200 font-inter font-semibold "
           onClick={services}
         >
           <h5>Services</h5>
         </span>
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-gray-200 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-gray-200 font-inter font-semibold "
           onClick={about}
         >
           <h5>About</h5>
         </span>
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-gray-200 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-gray-200 font-inter font-semibold "
           onClick={mentorship}
         >
           <h5>Mentorship</h5>
         </span>
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-green-500 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-green-500 font-inter font-semibold "
           onClick={contact}
         >
           <h5>Get in touch</h5>
         </span>
         <span
-          className="p-2.5 sm:text-[24px] text-[35px] text-green-500 font-inter font-semibold "
+          className="p-2.5 sm:text-[24px] pointer text-[35px] text-green-500 font-inter font-semibold "
           onClick={bookACall}
         >
           <h5>Book a call</h5>
