6be097fc8b5610fb64e52ad911d7e1786d755764
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-12 15:41:28 +0100

    modifing admin dashboard added left panel navbar pages funtion

--- src/Components/AdminDasboard/AdminDasbord.tsx
+++ src/Components/AdminDasboard/AdminDasbord.tsx
@@ -1,14 +1,79 @@
+import { useState } from "react";
 import LeftNevBar from "./LeftNevBar";
 import Dashboard from "./Navigation/Dashboard/Dashboard";
 import Bookings from "./Navigation/Booking/Bookings";
 function AdminDasboard() {
+  const [renderDashboard, setRenderDashboard] = useState<boolean>(true);
+  const [renderBooking, setRenderBooking] = useState<boolean>(false);
+  const [renderMentorship, setRenderMentorship] = useState<boolean>(false);
+  const [renderJobInquires, setRenderJobInquires] = useState<boolean>(false);
+  const [renderContent, setRenderContent] = useState<boolean>(false);
+  const [renderSettings, setRenderSettings] = useState<boolean>(false);
+  function toDashboard() {
+    setRenderSettings(false);
+    setRenderContent(false);
+    setRenderJobInquires(false);
+    setRenderMentorship(false);
+    setRenderBooking(false);
+    setRenderDashboard(true);
+  }
+  function toBooking() {
+    setRenderSettings(false);
+    setRenderContent(false);
+    setRenderJobInquires(false);
+    setRenderMentorship(false);
+    setRenderDashboard(false);
+    setRenderBooking(true);
+  }
+  function toMentorship() {
+    setRenderSettings(false);
+    setRenderContent(false);
+    setRenderJobInquires(false);
+    setRenderBooking(false);
+    setRenderDashboard(false);
+    setRenderMentorship(true);
+  }
+  function toJobinquires() {
+    setRenderSettings(false);
+    setRenderContent(false);
+    setRenderMentorship(false);
+    setRenderBooking(false);
+    setRenderDashboard(false);
+    setRenderJobInquires(true);
+  }
+  function toContent() {
+    setRenderSettings(false);
+    setRenderJobInquires(false);
+    setRenderMentorship(false);
+    setRenderBooking(false);
+    setRenderDashboard(false);
+    setRenderContent(true);
+  }
+  function toSettings() {
+    setRenderContent(false);
+    setRenderJobInquires(false);
+    setRenderMentorship(false);
+    setRenderBooking(false);
+    setRenderDashboard(false);
+    setRenderSettings(true);
+  }
+  function logout() {}
   return (
     <div className=" p-10 max-w-full min-w-fit  h-full bg-[#f8ffff] ">
       <div className="flex gap-10 h-fit w-full">
         {/**sideber left*/}
-        <LeftNevBar />
+        <LeftNevBar
+          toDashboard={toDashboard}
+          toBooking={toBooking}
+          toContent={toContent}
+          toJobinquires={toJobinquires}
+          toMentorship={toMentorship}
+          toSettings={toSettings}
+          logout={logout}
+        />
         {/**center content*/}
-        <Bookings />
+        {renderDashboard && <Dashboard />}
+        {renderBooking && <Bookings />}
       </div>
     </div>
   );
