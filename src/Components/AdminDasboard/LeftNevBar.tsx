bc091c2e36c7e82b6d8e8e33f19518cf72c0a3a2
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-12 15:41:28 +0100

    modifing admin dashboard added left panel navbar pages funtion

--- src/Components/AdminDasboard/LeftNevBar.tsx
+++ src/Components/AdminDasboard/LeftNevBar.tsx
@@ -1,5 +1,216 @@
+import { useRef, useState } from "react";
 import LogoImg from "/images/logo.png";
-function LeftNevBar() {
+type NavigationControl = {
+  toDashboard: () => void;
+  toBooking: () => void;
+  toMentorship: () => void;
+  toJobinquires: () => void;
+  toContent: () => void;
+  toSettings: () => void;
+  logout: () => void;
+};
+function LeftNevBar(props: NavigationControl) {
+  const dashboardRef = useRef<HTMLButtonElement | null>(null);
+  const bookingsRef = useRef<HTMLButtonElement | null>(null);
+  const mentorshipRef = useRef<HTMLButtonElement | null>(null);
+  const jobInquiresRef = useRef<HTMLButtonElement | null>(null);
+  const contentRef = useRef<HTMLButtonElement | null>(null);
+  const settingsRef = useRef<HTMLButtonElement | null>(null);
+  function toDashboard() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    //remove
+    dashboardRef.current.classList.remove("text-[#757575]");
+    bookingsRef.current.classList.remove("bg-primary-green");
+    bookingsRef.current.classList.remove("text-white");
+    bookingsRef.current.classList.add("text-[#757575]");
+    mentorshipRef.current.classList.remove("bg-primary-green");
+    mentorshipRef.current.classList.remove("text-white");
+    mentorshipRef.current.classList.add("text-[#757575]");
+    jobInquiresRef.current.classList.remove("bg-primary-green");
+    jobInquiresRef.current.classList.remove("text-white");
+    jobInquiresRef.current.classList.add("text-[#757575]");
+    contentRef.current.classList.remove("bg-primary-green");
+    contentRef.current.classList.remove("text-white");
+    contentRef.current.classList.add("text-[#757575]");
+    settingsRef.current.classList.remove("bg-primary-green");
+    settingsRef.current.classList.remove("text-white");
+    settingsRef.current.classList.add("text-[#757575]");
+    //add
+    dashboardRef.current.classList.add("bg-primary-green");
+    dashboardRef.current.classList.add("text-white");
+    //func call
+    props.toDashboard();
+  }
+  function toBooking() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    //remove
+    bookingsRef.current.classList.remove("text-[#757575]");
+    dashboardRef.current.classList.remove("bg-primary-green");
+    dashboardRef.current.classList.remove("text-white");
+    dashboardRef.current.classList.add("text-[#757575]");
+    mentorshipRef.current.classList.remove("bg-primary-green");
+    mentorshipRef.current.classList.remove("text-white");
+    mentorshipRef.current.classList.add("text-[#757575]");
+    jobInquiresRef.current.classList.remove("bg-primary-green");
+    jobInquiresRef.current.classList.remove("text-white");
+    jobInquiresRef.current.classList.add("text-[#757575]");
+    contentRef.current.classList.remove("bg-primary-green");
+    contentRef.current.classList.remove("text-white");
+    contentRef.current.classList.add("text-[#757575]");
+    settingsRef.current.classList.remove("bg-primary-green");
+    settingsRef.current.classList.remove("text-white");
+    settingsRef.current.classList.add("text-[#757575]");
+    //add
+    bookingsRef.current.classList.add("bg-primary-green");
+    bookingsRef.current.classList.add("text-white");
+    //func call
+    props.toBooking();
+  }
+  function toMentorship() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    //remove
+    mentorshipRef.current.classList.remove("text-[#757575]");
+    dashboardRef.current.classList.remove("bg-primary-green");
+    dashboardRef.current.classList.remove("text-white");
+    dashboardRef.current.classList.add("text-[#757575]");
+    bookingsRef.current.classList.remove("bg-primary-green");
+    bookingsRef.current.classList.remove("text-white");
+    bookingsRef.current.classList.add("text-[#757575]");
+    jobInquiresRef.current.classList.remove("bg-primary-green");
+    jobInquiresRef.current.classList.remove("text-white");
+    jobInquiresRef.current.classList.add("text-[#757575]");
+    contentRef.current.classList.remove("bg-primary-green");
+    contentRef.current.classList.remove("text-white");
+    contentRef.current.classList.add("text-[#757575]");
+    settingsRef.current.classList.remove("bg-primary-green");
+    settingsRef.current.classList.remove("text-white");
+    settingsRef.current.classList.add("text-[#757575]");
+    //add
+    mentorshipRef.current.classList.add("bg-primary-green");
+    mentorshipRef.current.classList.add("text-white");
+    //func call
+    props.toMentorship();
+  }
+  function toJobinquires() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    jobInquiresRef.current.classList.remove("text-[#757575]");
+    dashboardRef.current.classList.remove("bg-primary-green");
+    dashboardRef.current.classList.remove("text-white");
+    dashboardRef.current.classList.add("text-[#757575]");
+    bookingsRef.current.classList.remove("bg-primary-green");
+    bookingsRef.current.classList.remove("text-white");
+    bookingsRef.current.classList.add("text-[#757575]");
+    mentorshipRef.current.classList.remove("bg-primary-green");
+    mentorshipRef.current.classList.remove("text-white");
+    mentorshipRef.current.classList.add("text-[#757575]");
+    contentRef.current.classList.remove("bg-primary-green");
+    contentRef.current.classList.remove("text-white");
+    contentRef.current.classList.add("text-[#757575]");
+    settingsRef.current.classList.remove("bg-primary-green");
+    settingsRef.current.classList.remove("text-white");
+    settingsRef.current.classList.add("text-[#757575]");
+    //add
+    jobInquiresRef.current.classList.add("bg-primary-green");
+    jobInquiresRef.current.classList.add("text-white");
+    //func call
+    props.toJobinquires();
+  }
+  function toContent() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    contentRef.current.classList.remove("text-[#757575]");
+    dashboardRef.current.classList.remove("bg-primary-green");
+    dashboardRef.current.classList.remove("text-white");
+    dashboardRef.current.classList.add("text-[#757575]");
+    bookingsRef.current.classList.remove("bg-primary-green");
+    bookingsRef.current.classList.remove("text-white");
+    bookingsRef.current.classList.add("text-[#757575]");
+    mentorshipRef.current.classList.remove("bg-primary-green");
+    mentorshipRef.current.classList.remove("text-white");
+    mentorshipRef.current.classList.add("text-[#757575]");
+    jobInquiresRef.current.classList.remove("bg-primary-green");
+    jobInquiresRef.current.classList.remove("text-white");
+    jobInquiresRef.current.classList.add("text-[#757575]");
+    settingsRef.current.classList.remove("bg-primary-green");
+    settingsRef.current.classList.remove("text-white");
+    settingsRef.current.classList.add("text-[#757575]");
+    //add
+    contentRef.current.classList.add("bg-primary-green");
+    contentRef.current.classList.add("text-white");
+    //func call
+    props.toContent();
+  }
+  function toSettings() {
+    if (
+      !dashboardRef.current ||
+      !bookingsRef.current ||
+      !mentorshipRef ||
+      !jobInquiresRef.current ||
+      !contentRef.current ||
+      !settingsRef.current
+    )
+      return;
+    settingsRef.current.classList.remove("text-[#757575]");
+    dashboardRef.current.classList.remove("bg-primary-green");
+    dashboardRef.current.classList.remove("text-white");
+    dashboardRef.current.classList.add("text-[#757575]");
+    bookingsRef.current.classList.remove("bg-primary-green");
+    bookingsRef.current.classList.remove("text-white");
+    bookingsRef.current.classList.add("text-[#757575]");
+    mentorshipRef.current.classList.remove("bg-primary-green");
+    mentorshipRef.current.classList.remove("text-white");
+    mentorshipRef.current.classList.add("text-[#757575]");
+    contentRef.current.classList.remove("bg-primary-green");
+    contentRef.current.classList.remove("text-white");
+    contentRef.current.classList.add("text-[#757575]");
+    jobInquiresRef.current.classList.remove("bg-primary-green");
+    jobInquiresRef.current.classList.remove("text-white");
+    jobInquiresRef.current.classList.add("text-[#757575]");
+    //add
+    settingsRef.current.classList.add("bg-primary-green");
+    settingsRef.current.classList.add("text-white");
+    //func call
+    props.toSettings();
+  }
   return (
     <div className="w-[25%] min-w-73.5 flex flex-col bg-[#FFFFFF] p-10 rounded-[10px]">
       <div className="flex gap-2 items-center">
@@ -15,33 +226,57 @@ function LeftNevBar() {
       </div>
       {/**navber */}
       <div className="mt-10 flex flex-col gap-4">
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5 bg-primary-green text-white rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5 bg-primary-green text-white rounded-xl transition-all pointer"
+          ref={dashboardRef}
+          onClick={toDashboard}
+        >
           <i className="fa fa-home font-light text-[24px]"></i>
           <h5 className="font-inter font-medium text-[18px]">Dashboard</h5>
         </button>
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl transition-all pointer"
+          ref={bookingsRef}
+          onClick={toBooking}
+        >
           <i className="fas fa-calendar-alt font-light text-[24px]"></i>
           <h5 className="font-inter font-normal text-[18px]">Bookings</h5>
         </button>
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl transition-all pointer"
+          ref={mentorshipRef}
+          onClick={toMentorship}
+        >
           <i className="fas fa-user-graduate  text-[24px]"></i>
           <h5 className="font-inter font-normal text-[18px]">Mentorship</h5>
         </button>
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl transition-all pointer"
+          ref={jobInquiresRef}
+          onClick={toJobinquires}
+        >
           <i className="fas fa-briefcase  text-[24px]"></i>
           <h5 className="font-inter font-normal text-[18px]">Job inquires</h5>
         </button>
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl transition-all pointer"
+          ref={contentRef}
+          onClick={toContent}
+        >
           <i className="fas fa-cube  text-[24px]"></i>
           <h5 className="font-inter font-normal text-[18px]">Content</h5>
         </button>
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl">
+        <button
+          className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#757575] rounded-xl transition-all pointer"
+          ref={settingsRef}
+          onClick={toSettings}
+        >
           <i className="fas fa-sliders  text-[24px] rotate-90"></i>
-          <h5 className="font-inter font-normal text-[18px]">Settingss</h5>
+          <h5 className="font-inter font-normal text-[18px]">Settings</h5>
         </button>
       </div>
       <div className="mt-22.5">
-        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#C0392B] rounded-xl">
+        <button className="w-full h-12 flex items-center gap-2.5 p-3 pl-2.5 pr-2.5  text-[#C0392B] rounded-xl ">
           <i className="fas fa-right-from-bracket font-extralight  text-[24px]"></i>
           <h5 className="font-inter font-normal text-[18px]">Logout</h5>
         </button>
