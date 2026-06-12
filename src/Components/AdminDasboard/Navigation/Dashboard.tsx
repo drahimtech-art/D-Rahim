fcce60188f64db79fa641bfc73fd122dd2b8dd7a
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-12 12:14:21 +0100

    creating admin dashboard created on navigation dashboard

---
+++ src/Components/AdminDasboard/Navigation/Dashboard.tsx
@@ -0,0 +1,52 @@
+import TopBar from "../TopBar";
+import TopContent from "../TopContent";
+import RecentActivities from "../RecentActivities";
+import Alert from "../Alert";
+function Dashboard() {
+  return (
+    <div className="w-full h-full flex flex-col gap-7 ">
+      {/**top bar */}
+      <TopBar />
+      {/**Top Content */}
+      <TopContent />
+      {/**booking & selas chat */}
+      <div className="w-full grid grid-cols-2 h-75.75 gap-6.75">
+        <div className="w-full h-full pl-5 flex flex-col pr-5 p-3.5 bg-[#FFFFFF] rounded-[20px]">
+          <div className="pt-3.5 pb-3.5 flex items-center">
+            <span className="mr-auto">
+              <h5 className="font-inet font-medium min18pxMax20px">Bookings</h5>
+            </span>
+            <span className="w-fit h-7.5 flex gap-1.5 items-center pl-2 pr-2 pt-1.5 pb-1.5 border border-gray-500 rounded-[30px]">
+              <h5>Weekly</h5>
+              <i className="fa fa-angle-down text-gray-500"></i>
+            </span>
+          </div>
+          {/** */}
+          <div className="mt-4.5 w-full h-full bg-gray-500"></div>
+        </div>
+        {/**content */}
+        <div className="w-full h-full pl-5 pr-5 p-3.5 flex flex-col bg-[#FFFFFF] rounded-[20px]">
+          <div className="pt-3.5 pb-3.5  flex items-center">
+            <span className="mr-auto">
+              <h5 className="font-inet font-medium min18pxMax20px">Sales</h5>
+            </span>
+            <span className="w-fit h-7.5 flex gap-1.5 items-center pl-2 pr-2 pt-1.5 pb-1.5 border border-gray-500 rounded-[30px]">
+              <h5>Weekly</h5>
+              <i className="fa fa-angle-down text-gray-500"></i>
+            </span>
+          </div>
+          {/** */}
+          <div className="mt-4.5 w-full h-full bg-gray-500"></div>
+        </div>
+      </div>
+      {/**end content */}
+      <div className="flex w-full h-68.5 gap-6.75">
+        {/**reacent activity */}
+        <RecentActivities />
+        {/**alerts */}
+        <Alert />
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
--- src/Components/AdminDasboard/TopContent.tsx
+++ src/Components/AdminDasboard/TopContent.tsx
@@ -2,7 +2,7 @@ function TopContent() {
   return (
     <div className="w-full h-fit grid grid-cols-4 gap-6.75">
       {/**content */}
-      <div className="flex flex-col gap-1 w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
+      <div className="flex flex-col  w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
         <span>
           <h5 className="font-inter font-medium text-[16px] text-[#1C1C1E]">
             Total Bookings
@@ -12,11 +12,11 @@ function TopContent() {
           <i className="fa fa-calendar-alt text-[30px] text-[#11AC76]"></i>
         </span>
         <span>
-          <h5 className="font-inter font-semibold min28pxMax30">12</h5>
+          <h5 className="font-inter font-semibold min28pxMax30 -mt-5">12</h5>
         </span>
       </div>
       {/**content */}
-      <div className="flex flex-col gap-1 w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
+      <div className="flex flex-col  w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
         <span>
           <h5 className="font-inter font-medium text-[16px] text-[#1C1C1E]">
             Mentorship Sales
@@ -26,11 +26,11 @@ function TopContent() {
           <i className="fa fa-user-graduate text-[30px] text-[#11AC76]"></i>
         </span>
         <span>
-          <h5 className="font-inter font-semibold min28pxMax30">8</h5>
+          <h5 className="font-inter font-semibold min28pxMax30 -mt-5">8</h5>
         </span>
       </div>
       {/**content */}
-      <div className="flex flex-col gap-1 w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
+      <div className="flex flex-col  w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
         <span>
           <h5 className="font-inter font-medium text-[16px] text-[#1C1C1E]">
             Job Requests
@@ -40,11 +40,11 @@ function TopContent() {
           <i className="fa fa-briefcase text-[30px] text-[#11AC76]"></i>
         </span>
         <span>
-          <h5 className="font-inter font-semibold min28pxMax30">6</h5>
+          <h5 className="font-inter font-semibold min28pxMax30 -mt-5">6</h5>
         </span>
       </div>
       {/**content */}
-      <div className="flex flex-col gap-1 w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
+      <div className="flex flex-col  w-full pl-5 pr-5 p-3.5 bg-[#FFFFFF] rounded-[14px]">
         <span>
           <h5 className="font-inter font-medium text-[16px] text-[#1C1C1E]">
             Total Users
@@ -54,7 +54,7 @@ function TopContent() {
           <i className="fa fa-user-friends text-[30px] text-[#11AC76]"></i>
         </span>
         <span>
-          <h5 className="font-inter font-semibold min28pxMax30">20</h5>
+          <h5 className="font-inter font-semibold min28pxMax30 -mt-5">20</h5>
         </span>
       </div>
     </div>
