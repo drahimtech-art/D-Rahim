a5f4abf58e0b79d1afc591178e72dd4c5723937a
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-13 19:50:07 +0100

    Reactoring Main home page / welcome page organizing frontend structure (folders&paths)

--- src/Components/Main_Home/GetInTouch/EnterDetailsAndMap.tsx
+++ src/Components/Main_Home/GetInTouch/EnterDetailsAndMap.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 function EnterDetailsAndMap() {
   const [date, setDate] = useState(new Date());
-  console.log(setDate)
+  console.log(setDate);
   return (
     <div className="flex min-[1000px]:pl-10 min-[1000px]:pr-10 pl-5 pr-5 min-[1000px]:mt-15 mt-10 min-[1000px]:gap-10 gap-5">
       <div className="min-[1000px]:w-[60%] min-[1000px]:max-w-232">
@@ -10,49 +10,53 @@ function EnterDetailsAndMap() {
           <h5>Enter Details</h5>
         </span>
         <div className=" flex flex-col min-[1000px]:max-w-232 min-[1000px]:gap-1.5 gap-5 min-[1000px]:mt-0 mt-10">
-          <span className=" font-inter">
+          <span className="flex flex-col font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Full Name
             </h5>
             <input className="border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
           </span>
-          <span className=" font-inter">
+          <span className="flex flex-col font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Job Title
             </h5>
             <input className="border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
           </span>
-          <span className=" font-inter">
+          <span className="flex flex-col font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Business Email
             </h5>
             <input className="border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
           </span>
-          <span className=" font-inter">
+          <span className="flex flex-col font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Phone Number
             </h5>
             <input className="border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
           </span>
-          <span className=" font-inter">
+          <span className="flex flex-col font-inter">
             <h5 className="ourWorkHeadMenu font-inter font-medium">
               Company Name
             </h5>
             <input className="border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
           </span>
-          <span className=" font-inter">
-            <h5 className="ourWorkHeadMenu font-inter font-medium">
-              Tell us about your project
-            </h5>
+          <span className="flex flex-col h-full font-inter">
+            <span>
+              <h5 className="ourWorkHeadMenu font-inter font-medium">
+                Tell us about your project
+              </h5>
+            </span>
             <span>
               <input className="min-[1000px]:inline-block hidden h-41.75 min-[1000px]:h-full border-2 min-[1000px]:border-0 pl-4 sm:min-h-12  min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
               <textarea className="min-[1000px]:hidden inline-block  h-41.75  border-2 min-[1000px]:border-0 pl-4 sm:min-h-12 min-[1000px]:h-full min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></textarea>
             </span>
           </span>
-          <span className=" font-inter">
-            <h5 className="ourWorkHeadMenu font-inter font-medium">
-              How did you find D'rahim tech innovation?
-            </h5>
+          <span className="flex flex-col h-full font-inter mt-0 min-[1000px]:mt-5">
+            <span>
+              <h5 className="ourWorkHeadMenu font-inter font-medium">
+                How did you find D'rahim tech innovation?
+              </h5>
+            </span>
             <span>
               <input className="min-[1000px]:inline-block hidden h-41.75 min-[1000px]:h-full border-2 min-[1000px]:border-0 pl-4 sm:min-h-12  min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></input>
               <textarea className="min-[1000px]:hidden inline-block  h-41.75 min-[1000px]:h-full border-2 min-[1000px]:border-0 pl-4  min-[1000px]:border-b-2 border-gray-600 w-full p-2.5 min-[620px]:p-0 min-[1000px]:pb-7 min-[1000px]:mt-6 rounded-2xl min-[1000px]:rounded-none"></textarea>
