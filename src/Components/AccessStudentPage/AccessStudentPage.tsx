cf3b8c7ab0b6cfbf5a03b5cb408fb4ad12ad8848
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-15 05:05:11 +0100

    Creating student access page created and modified dashboard page on students navigation page

--- src/Components/AccessStudentPage/AccessStudentPage.tsx
+++ src/Components/AccessStudentPage/AccessStudentPage.tsx
@@ -1,17 +1,24 @@
 import Top from "./Top";
 import LeftNevBar from "./LeftNevBar";
+import Dashboard from "./Dashboard/Dashboard";
 function AccessStudentPage() {
   return (
-    <div className="w-full h-screen max-h-screen  p-10  ">
+    <div className="w-full min-w-300 h-screen  max-h-screen overflow-hidden  p-10  ">
       <div className="w-full h-full max-h-full flex flex-col ">
         {/**Top */}
         <Top />
         {/**content */}
-        <div className="w-full h-full min-h-fit  max-h-full flex mt-10 ">
+        <div className="w-full max-w-full flex min-h-0 max-h-full gap-10 mt-10 ">
           {/**left navigation panel */}
-          <div className=" min-w-45 w-full max-w-80.75 h-full border-r border-gray-500">
+          <div className=" min-w-45 w-full max-w-80.75 h-full shrink-0 border-r border-gray-500 overflow-hidden">
             <div className="w-full h-full max-w-68.5 pt-5">
               <LeftNevBar />
+            </div>
+          </div>
+          {/**main content */}
+          <div className="w-full  h-full ">
+            <div className="w-full h-full overflow-y-auto">
+              <Dashboard />
             </div>
           </div>
         </div>
