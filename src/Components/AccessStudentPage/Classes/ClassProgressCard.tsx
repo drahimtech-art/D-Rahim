cb84d94f5949926fc419b1fdd94d67213aab2123
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-16 01:00:07 +0100

    creating access student page created class page (all classes)

---
+++ src/Components/AccessStudentPage/Classes/ClassProgressCard.tsx
@@ -0,0 +1,40 @@
+import classImge1 from "/images/class1.png";
+function ClassProgressBar() {
+  return (
+    <div className="flex items-center w-full gap-25.75">
+      <div className="flex items-center gap-3.25 w-full">
+        <span className="min-w-20.5 h-16.75">
+          <img className="w-full h-full" src={classImge1}></img>
+        </span>
+        {/**heading & sub heading */}
+        <span className="w-full h-full flex flex-col gap-1.25">
+          <h5 className="line-clamp-1 font-sans font-medium text-[16px]">
+            Ui/Ux Design Mentorship Bootcamp
+          </h5>
+          <h5 className="line-clamp-1 font-sans font-normal text-[16px]">
+            12 Modules - 24 Lessons
+          </h5>
+        </span>
+      </div>
+      {/**progress bar */}
+      <div className="flex flex-col w-full gap-1">
+        <h5 className="line-clamp-1 font-sans font-normal text-[16px]">
+          Progress
+        </h5>
+        <div className="w-full flex items-center  gap-2.5">
+          <span className="block w-full max-w-72.5 h-1.5 rounded-[3px] bg-[#787878]">
+            <span className="block w-[70%] h-full bg-[#0088FF] rounded-[3px]"></span>
+          </span>
+          <h5 className=" font-sans font-normal text-[16px]">75%</h5>
+        </div>
+      </div>
+      {/**action button */}
+      <button className="pointer p-2.5 min-w-26.75 flex justify-center items-center bg-[#11AC76] rounded-[30px]">
+        <h5 className=" font-sans font-medium text-[16px] text-[#FFFFFF]">
+          View Class
+        </h5>
+      </button>
+    </div>
+  );
+}
+export default ClassProgressBar;
--- src/Components/AccessStudentPage/Classes/Classes.tsx
+++ src/Components/AccessStudentPage/Classes/Classes.tsx
@@ -1,9 +1,35 @@
 import HeadContent from "./HeadContent";
+//
+/*
+import classImge1 from "/images/class1.png";
+import classImge2 from "/images/class2.png";
+import classImge3 from "/images/class5.png";
+import classImge4 from "/images/class6.png";
+import classImge5 from "/images/class3.png";
+import classImge6 from "/images/class7.png";
+import classImge7 from "/images/class4.png";
+*/
+import ClassProgressBar from "./ClassProgressCard";
 function Classes() {
   return (
     <div className="w-full h-full">
       {/**head content */}
       <HeadContent />
+      {/**content body*/}
+      <div className="flex flex-col gap-6.25">
+        {/**content */}
+        <ClassProgressBar />
+        {/**content */}
+        <ClassProgressBar />
+        {/**content */}
+        <ClassProgressBar />
+        {/**content */}
+        <ClassProgressBar />
+        {/**content */}
+        <ClassProgressBar />
+        {/**content */}
+        <ClassProgressBar />
+      </div>
     </div>
   );
 }
