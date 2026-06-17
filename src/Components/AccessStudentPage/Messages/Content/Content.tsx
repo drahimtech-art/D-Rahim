05bb4d83a85babc6ffd698f02d64a2aff79792ce
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-17 12:25:00 +0100

    modifing access students page modifing massage page modified conent page made contact sroll while keeping everything else fixed on the screen

--- src/Components/AccessStudentPage/Messages/Content/Content.tsx
+++ src/Components/AccessStudentPage/Messages/Content/Content.tsx
@@ -1,9 +1,9 @@
 import Contact from "./Contact/Contact";
 function Content() {
   return (
-    <div className="mt-7.5 w-full h-full flex b">
+    <div className="mt-7.5 w-full h-full max-h-full overflow-hidden flex">
       {/**left contact panel */}
-      <div className="w-[30%] overflow-hidden max-w-80.25  border-r border-[#11AC76]">
+      <div className="w-[30%] h-full max-h-full overflow-y-auto max-w-80.25  border-r border-[#11AC76]">
         <Contact />
       </div>
     </div>
--- src/Components/AccessStudentPage/Messages/Messages.tsx
+++ src/Components/AccessStudentPage/Messages/Messages.tsx
@@ -2,11 +2,13 @@ import Head from "./Head";
 import Content from "./Content/Content";
 function Messages() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full max-h-full h-full overflow-hidden ">
       {/**head */}
       <Head />
       {/**conatent */}
-      <Content />
+      <div className="w-full h-full">
+        <Content />
+      </div>
     </div>
   );
 }
