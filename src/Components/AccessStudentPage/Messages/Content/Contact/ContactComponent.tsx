b26aaf6e68e82341ecd29db30a8dc395c2d2251c
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-23 23:15:41 +0200

    implimenting backend apis modifing access students page added message hsitory api on contact component to render user and contact/connection chat history

--- src/Components/AccessStudentPage/Messages/Content/Contact/ContactComponent.tsx
+++ src/Components/AccessStudentPage/Messages/Content/Contact/ContactComponent.tsx
@@ -1,4 +1,6 @@
 import { StudentsAppData } from "../../../../ContextApi/StudentsApi";
+import { SocketApi } from "../../../../ContextApi/SocketApi";
+import { useEffect } from "react";
 import contactImg1 from "/images/contact.png";
 type Connections = {
   contactFirstName: string;
@@ -22,9 +24,12 @@ type ChatContact = {
   messages: Messages[];
 };
 function ContactComponent({ connectionInfo }: { connectionInfo: Connections }) {
+  const serverPort = import.meta.env.VITE_SERVER_PORT;
   const userDetails = StudentsAppData();
   if (!userDetails) return;
-  const { setChatContact, chatContact } = userDetails;
+  const socketApi = SocketApi();
+  const { socket } = socketApi;
+  const { setChatContact, chatContact, userInfo } = userDetails;
   function obenChatBox() {
     const data: ChatContact = {
       contactId: connectionInfo.contactId,
@@ -54,6 +59,45 @@ function ContactComponent({ connectionInfo }: { connectionInfo: Connections }) {
         color: "black",
         backgroundColor: "transparent",
       };
+  //get messages on mount
+  useEffect(() => {
+    if (!userInfo.connectionId || !connectionInfo.contactId) return;
+    const getContactChatHistory = async (
+      connectionId: string,
+      contactId: string,
+    ) => {
+      const CLIENT_KEY = "CLIENT_KEY";
+      const data = localStorage.getItem(CLIENT_KEY);
+      try {
+        if (!data || data === "null") throw new Error("Access key not found");
+        const key = JSON.parse(data);
+        const requstBody = {
+          connectionId: connectionId,
+          contactId: contactId,
+        };
+        const requst = await fetch(
+          `${serverPort}/connection/contact/messages`,
+          {
+            method: "POST",
+            credentials: "include",
+            headers: {
+              "X-Frontend-Key": `${key}`,
+            },
+            body: JSON.stringify(requstBody),
+          },
+        );
+        const responds = await requst.json();
+        if (responds.ok) {
+          console.log(responds.chatHistory);
+        } else {
+          console.log(responds.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getContactChatHistory(userInfo.connectionId, connectionInfo.contactId);
+  }, [connectionInfo.contactId, userInfo.connectionId]);
   return (
     <div
       className="w-full h-fit p-2.5 pr-5.5 flex items-center gap-2.5  rounded-2xl pointer transition-all"
