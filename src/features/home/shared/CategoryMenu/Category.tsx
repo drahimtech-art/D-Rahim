98e96f16f67402799cde451315e563ec31c1c3cc
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-07-25 12:40:57 +0200

    created random data picker for ourwork project data the flow takes 3 caltigorys and a list for each caltigory it would randomly pick 2 data from each list and return 6 new items for each caltigory results 3 list  -> 1 list = 6 items; 2 list = 6 items; 3 list = 6 items;

--- src/features/home/shared/CategoryMenu/Category.tsx
+++ src/features/home/shared/CategoryMenu/Category.tsx
@@ -63,38 +63,68 @@ function Category() {
     while (allProjects.length < 3) {
       // while allProjects is not index on 3 counts loop
       let count = 0; // full (3) index count that makes on allProjects index; 3 count = 1 allProjects index
-      let guessNumber; // guessNumber
+      let guessNumber; // guessNumber // guess number range between 0 - 2 as 0 stands for moblie design 1 for website design 2 for branding
       const filteringList: OurWorkData[] = []; // list of getted values to make one full allProjects index list length can be below 6 to make one full index
       function getFullList() {
         do {
           //guess a random number between 0 - 2
-          const firstGuess = Math.floor(Math.random() * 3);
+          const firstGuess = Math.floor(Math.random() * 3); // guess a number to use that number would be used to pick a catigory to loop and get data from
           guessNumber = firstGuess;
         } while (guessedList.includes(guessNumber)); // compare guessed number to guessed list since guessed list can only be 0, 1, 2, we guess in the range that hassent been guessed
-        guessedList.push(guessNumber);
-        console.log(guessedList);
+        guessedList.push(guessNumber); // push new guessed number that hasn't been guessed
+        //
         function getRandeomIndex(
           counter: number,
           sltIndex: number | undefined,
           list: OurWorkData[],
           sltIndexInList: number[],
-        ) {
-          console.log("before pass", counter, sltIndex);
-          const index = Math.floor(Math.random() * list.length);
-          if (sltIndexInList.includes(index))
-            return (
-              console.log("return", sltIndex, counter),
-              getRandeomIndex(counter, sltIndex, list, sltIndexInList)
+          firstSubList: OurWorkData[],
+          secondSubList: OurWorkData[],
+        ): void {
+          const index = Math.floor(Math.random() * list.length); // guess a random index between 0 - data in list length
+          if (
+            sltIndexInList.includes(index) &&
+            sltIndexInList.length !== list.length // check if that data at guessed index hasent been added to list and if all index in list havent been gussed if conditions are meat don't guees a new number continue to flow
+          )
+            return getRandeomIndex(
+              counter,
+              sltIndex,
+              list,
+              sltIndexInList,
+              firstSubList,
+              secondSubList,
             );
-          sltIndex = index;
-          counter += 1;
-          sltIndexInList.push(index);
           console.log("pass", counter, sltIndex);
-          const item = list[index];
-          filteringList.push(item);
+          counter += 1;
+          if (sltIndexInList.length !== list.length) {
+            // if all index hassent been guss push the item at the current index
+            sltIndex = index;
+            sltIndexInList.push(index);
+            const item = list[index];
+            filteringList.push(item);
+          } else {
+            // else push random data betwwen othere list caltigory to meat list length
+            // fall back if all slttindexinlist was selected and no more index left
+            if (counter === 1) {
+              const firstPick =
+                firstSubList[Math.floor(Math.random() * firstSubList.length)];
+              filteringList.push(firstPick);
+            } else {
+              const secondPick =
+                secondSubList[Math.floor(Math.random() * secondSubList.length)];
+              filteringList.push(secondPick);
+            }
+          }
           if (counter !== 2) {
-            console.log("recall");
-            getRandeomIndex(counter, sltIndex, list, sltIndexInList);
+            // since am only getting 2 data data at i time check if i have gotten random data twice  as i pushed before i check so first push data -> counter = 1; check ondition;  second push data -> counter = 2; check condition;
+            getRandeomIndex(
+              counter,
+              sltIndex,
+              list,
+              sltIndexInList,
+              firstSubList,
+              secondSubList,
+            );
           }
         }
         if (guessNumber === 0 && moblieDesign.length !== 0) {
@@ -107,6 +137,8 @@ function Category() {
             selectedIndex,
             moblieDesign,
             indexPickedInMoblieDesign,
+            websiteDesign,
+            branding,
           );
         } else if (guessNumber === 1 && websiteDesign.length !== 0) {
           // websiteDesign
@@ -117,6 +149,8 @@ function Category() {
             selectedIndex,
             websiteDesign,
             indexPickedInWebSiteDesign,
+            moblieDesign,
+            branding,
           );
         } else if (guessNumber === 2 && branding.length !== 0) {
           // branding
@@ -127,6 +161,8 @@ function Category() {
             selectedIndex,
             branding,
             indexPickedInBranding,
+            moblieDesign,
+            websiteDesign,
           );
         }
         count += 1;
